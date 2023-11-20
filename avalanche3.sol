2a46b704b44bcbd838cc4ce13a4f32b859a43aff
Micko Alhwin Andrada <113717238+DKARCHDUKE@users.noreply.github.com>
2023-11-20 22:54:14 +0800

    Update avalanche3.sol

--- avalanche3.sol
+++ avalanche3.sol
@@ -4,21 +4,26 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts@4.9/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@4.9/access/Ownable.sol";
 
-contract mod3 is ERC20, Ownable {
-    constructor() ERC20("Avalanche", "AVAX") {
-        _mint(msg.sender, 20);
+contract micko is ERC20, Ownable {
+    event Mint(address indexed to, uint256 amount);
+    event Burn(address indexed from, uint256 amount);
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
+        emit Mint(to, amount);
     }
 
-    function burn(uint256 amount) public {
-        
-        _burn(msg.sender, amount);
+    function burn(uint256 amount) external {
+        _burn(_msgSender(), amount);
+        emit Burn(_msgSender(), amount);
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
-        return super.transfer(recipient, amount);
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        require(to != address(this), "Cannot transfer to the token contract");
+        _transfer(_msgSender(), to, amount);
+        return true;
     }
 }
